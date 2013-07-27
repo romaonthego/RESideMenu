d5fd4bfdc50a8c71ff5cadb1df6b4e1ef6c8bc68
Alex <kirualex@gmail.com>
2013-07-27 23:17:12 +0200

    added initFieldWithPlaceholder declaration

--- RESideMenu/RESideMenuItem.h
+++ RESideMenu/RESideMenuItem.h
@@ -47,13 +47,19 @@ typedef NS_ENUM(NSInteger, SideMenuItemType) {
 - (id)initWithTitle:(NSString *)title
              action:(void(^)(RESideMenu *menu, RESideMenuItem *item))action;
 
-- (id)initFieldWithPlaceholder:(NSString *)placeholder
-             doneAction:(void(^)(RESideMenu *menu, RESideMenuItem *item))action;
-
 - (id)initWithTitle:(NSString *)title
               image:(UIImage *)image
    highlightedImage:(UIImage *)highlightedImage
              imageAction:(void(^)(RESideMenu *menu, RESideMenuItem *item))imageAction
              action:(void(^)(RESideMenu *menu, RESideMenuItem *item))action;
+
+- (id)initFieldWithPlaceholder:(NSString *)placeholder
+                    doneAction:(void(^)(RESideMenu *menu, RESideMenuItem *item))action;
+
+- (id)initFieldWithPlaceholder:(NSString *)placeholder
+                         image:(UIImage *)image
+              highlightedImage:(UIImage *)highlightedImage
+                   imageAction:(void(^)(RESideMenu *menu, RESideMenuItem *item))imageAction
+                    doneAction:(void(^)(RESideMenu *menu, RESideMenuItem *item))action;
 
 @end
