3b06602e119b364e932690a9ba3f12ebeeea3075
Roman Efimov <romaonthego@gmail.com>
2013-07-29 12:11:35 -0500

    Refactor animation code

--- RESideMenu/AccelerationAnimation.h
+++ RESideMenu/AccelerationAnimation.h
@@ -17,18 +17,8 @@
 @protocol Evaluate;
 
 @interface AccelerationAnimation : CAKeyframeAnimation
-{
-}
 
-+ (id)animationWithKeyPath:(NSString *)keyPath
-	startValue:(double)startValue
-	endValue:(double)endValue
-	evaluationObject:(NSObject<Evaluate> *)evaluationObject
-	interstitialSteps:(NSUInteger)steps;
-
-- (void)calculateKeyFramesWithEvaluationObject:(NSObject<Evaluate> *)evaluationObject
-	startValue:(double)startValue
-	endValue:(double)endValue
-	interstitialSteps:(NSUInteger)steps;
++ (id)animationWithKeyPath:(NSString *)keyPath startValue:(double)startValue endValue:(double)endValue evaluationObject:(NSObject<Evaluate> *)evaluationObject interstitialSteps:(NSUInteger)steps;
+- (void)calculateKeyFramesWithEvaluationObject:(NSObject<Evaluate> *)evaluationObject startValue:(double)startValue endValue:(double)endValue interstitialSteps:(NSUInteger)steps;
 
 @end
--- RESideMenu/AccelerationAnimation.m
+++ RESideMenu/AccelerationAnimation.m
@@ -17,27 +17,16 @@
 
 @implementation AccelerationAnimation
 
-+ (id)animationWithKeyPath:(NSString *)keyPath
-	startValue:(double)startValue
-	endValue:(double)endValue
-	evaluationObject:(NSObject<Evaluate> *)evaluationObject
-	interstitialSteps:(NSUInteger)steps
++ (id)animationWithKeyPath:(NSString *)keyPath startValue:(double)startValue endValue:(double)endValue evaluationObject:(NSObject<Evaluate> *)evaluationObject interstitialSteps:(NSUInteger)steps
 {
 	id animation = [self animationWithKeyPath:keyPath];
     
-	[animation
-		calculateKeyFramesWithEvaluationObject:evaluationObject
-		startValue:(double)startValue
-		endValue:(double)endValue
-		interstitialSteps:steps];
+	[animation calculateKeyFramesWithEvaluationObject:evaluationObject startValue:(double)startValue endValue:(double)endValue interstitialSteps:steps];
 	
 	return animation;
 }
 
-- (void)calculateKeyFramesWithEvaluationObject:(NSObject<Evaluate> *)evaluationObject
-	startValue:(double)startValue
-	endValue:(double)endValue
-	interstitialSteps:(NSUInteger)steps
+- (void)calculateKeyFramesWithEvaluationObject:(NSObject<Evaluate> *)evaluationObject startValue:(double)startValue endValue:(double)endValue interstitialSteps:(NSUInteger)steps
 {
 	NSUInteger count = steps + 2;
 	
@@ -46,8 +35,7 @@
 	double progress = 0.0;
 	double increment = 1.0 / (double)(count - 1);
 	NSUInteger i;
-	for (i = 0; i < count; i++)
-	{
+	for (i = 0; i < count; i++) {
 		double value = startValue + [evaluationObject evaluateAt:progress] * (endValue - startValue);
 		[valueArray addObject:[NSNumber numberWithDouble:value]];
 		
