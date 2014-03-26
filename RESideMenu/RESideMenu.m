a1f38e185f889201fc6392b66dd09a201e17d2df
Roman Efimov <romefimov@gmail.com>
2014-03-26 09:38:58 -0700

    Remove unused methods

--- RESideMenu/RESideMenu.m
+++ RESideMenu/RESideMenu.m
@@ -171,26 +171,6 @@
     }
 }
 
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-}
-
-- (void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
-}
-
-- (void)viewWillDisappear:(BOOL)animated
-{
-    [super viewWillDisappear:animated];
-}
-
-- (void)viewDidDisappear:(BOOL)animated
-{
-    [super viewDidDisappear:animated];
-}
-
 #pragma mark -
 
 - (void)presentLeftMenuViewController
