d278b76402e7c706cce735d492a3c20d58b5d3ef
Guido Marucci Blas <guidomb@gmail.com>
2015-05-20 15:51:35 +0800

    Adds support for Carthage.

---
+++ RESideMenuTests/RESideMenuTests.m
@@ -0,0 +1,40 @@
+//
+//  RESideMenuTests.m
+//  RESideMenuTests
+//
+//  Created by Guido Marucci Blas on 5/20/15.
+//  Copyright (c) 2015 Roman Efimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface RESideMenuTests : XCTestCase
+
+@end
+
+@implementation RESideMenuTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
